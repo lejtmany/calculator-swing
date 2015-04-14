0f168c3840b9c686ad22acac020a16c6e33424e3
lejtmany <lejtmany@gmail.com>
2015-04-13 23:15:23 -0400

    added functionality so that more closely resembles microsoft calculator. Added MS, MR, M+, M-, CE, reciprocal, square root, CE, left arrow, negative toggle back end not yet implemented

--- Calculator/src/com/lejtman/Calculator.java
+++ Calculator/src/com/lejtman/Calculator.java
@@ -5,7 +5,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.script.ScriptEngine;
+import java.text.DecimalFormat;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,91 +16,222 @@ import javax.swing.JPanel;
  * @author lejtman
  */
 public class Calculator extends JFrame {
-    
-    JLabel display;
-    JPanel buttonPad;
-    String equationString = "";
-    ScriptEngine engine;
-    MathExpressionParser parser;
-    
+
+    private JLabel topDisplay, entryDisplay;
+    private JPanel displayPanel, buttonPad;
+    private String memory, lastEntry, equationString;
+    private MathExpressionParser parser;
+    private boolean entryIsAnswer;
+
     public Calculator() {
         parser = new MathExpressionParser();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 600);
         this.setLayout(new BorderLayout());
-        display = new JLabel(" ");
+        displayPanel = new JPanel(new BorderLayout());
+        topDisplay = new JLabel(" ");
+        entryDisplay = new JLabel(" ");
         buttonPad = new JPanel();
         addButtons();
         buttonPad.setLayout(new GridLayout(5, 5));
-        this.add(display, BorderLayout.NORTH);
-        this.add(buttonPad, BorderLayout.CENTER);        
+        displayPanel.add(topDisplay, BorderLayout.NORTH);
+        displayPanel.add(entryDisplay, BorderLayout.CENTER);
+        this.add(displayPanel, BorderLayout.NORTH);
+        this.add(buttonPad, BorderLayout.CENTER);
         this.pack();
         this.setVisible(true);
     }
-    
+
     private void addButtons() {
-        String[] operators = {"+", "-", "*", "/", "."};
+        //operator buttons
+        String[] operators = {"+", "-", "*", "/"};
         for (String operator : operators) {
-            addButton(operator, new StringTextAdder());
+            addButton(operator, new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    topDisplay.setText(topDisplay.getText() + " " + entryDisplay.getText() + " " + e.getActionCommand());
+                    lastEntry = entryDisplay.getText();
+                    entryDisplay.setText(" ");
+                    entryIsAnswer = false;
+                }
+            });
         }
+
+        //numeric buttons
         for (int i = 0; i < 10; i++) {
-            addButton(i + "", new StringTextAdder());
+            addButton(i + "", new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (entryIsAnswer) {
+                        entryDisplay.setText(" ");
+                        topDisplay.setText(" ");
+                        entryIsAnswer = false;
+                    }
+                    entryDisplay.setText(entryDisplay.getText() + e.getActionCommand());
+                }
+            });
         }
-        addButton("C", new ActionListener() {
-            
+
+        addButton(".", new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
-                equationString = "";
-                updateDisplay(equationString);
+                if (entryIsAnswer) {
+                    entryDisplay.setText(" ");
+                    topDisplay.setText(" ");
+                    entryIsAnswer = false;
+                }
+                String entryText = entryDisplay.getText();
+                if (!entryText.contains(".")) {
+                    entryDisplay.setText(entryText + ".");
+                }
             }
         });
-        
-        addButton("=", new ActionListener() {
-            
+
+        addButton("C", new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{
-                Double result = parser.parse(equationString);
-                updateDisplay("" + ((result.intValue() == result)?  result.intValue() + "": result));             
+                topDisplay.setText(" ");
+                entryDisplay.setText(" ");
+                memory = "";
+                lastEntry = "";
+            }
+        });
+
+        addButton("CE", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                entryDisplay.setText(" ");
+            }
+        });
+
+        addButton("MS", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                memory = entryDisplay.getText();
+            }
+        });
+
+        addButton("MR", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DecimalFormat df = new DecimalFormat("#");
+                entryDisplay.setText(df.format(Double.parseDouble(memory)));
+            }
+        });
+
+        addButton("M+", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                memory = Double.parseDouble(memory) + Double.parseDouble(entryDisplay.getText()) + "";
+            }
+        });
+
+        addButton("M-", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                memory = Double.parseDouble(memory) - Double.parseDouble(entryDisplay.getText()) + "";
+            }
+        });
+
+        addButton("MC", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                memory = "";
+            }
+        });
+
+        //left arrow button
+        addButton("\u2190", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String entryText = entryDisplay.getText();
+                entryDisplay.setText(entryText.substring(0, entryText.length() - 1));
+            }
+        });
+
+        //square root button
+        addButton("\u221A", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                topDisplay.setText(String.format("%s sqrt(%s)", topDisplay.getText(), entryDisplay.getText()));
+                entryDisplay.setText(Math.sqrt(Double.parseDouble(entryDisplay.getText())) + "");
+                lastEntry = entryDisplay.getText();
+                entryIsAnswer = true;
+            }
+        });
+
+        addButton("%", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                entryDisplay.setText(Double.parseDouble(lastEntry) * (Double.parseDouble(entryDisplay.getText()) * .01) + "");
+                entryIsAnswer = true;
+            }
+        });
+
+        addButton("1/x", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                topDisplay.setText(String.format("%s reciproc(%s)", topDisplay.getText(), entryDisplay.getText()));
+                entryDisplay.setText(1 / Double.parseDouble(entryDisplay.getText()) + "");
+                entryIsAnswer = true;
+            }
+        });
+
+        //plus-minus sign
+        addButton("\u00B1", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String entryText = entryDisplay.getText();
+                if(entryText.trim().isEmpty())
+                    return;
+                if (entryText.startsWith("-")) {
+                    entryDisplay.setText(entryText.substring(1, entryText.length()));
+                } else {
+                    entryDisplay.setText("-" + entryText);
                 }
-                catch(IllegalArgumentException ex){
-                    updateDisplay(ex.getMessage());                  
+            }
+        });
+
+        addButton("=", new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Double result = parser.parse(equationString);
+                    entryDisplay.setText("" + ((result.intValue() == result) ? result.intValue() + "" : result));
+                } catch (IllegalArgumentException ex) {
+                    topDisplay.setText(ex.getMessage());
+                } finally {
+                    equationString = "";
                 }
-                finally{
-                   equationString = ""; 
-                }             
             }
         }
         );
-        
+
     }
-    
+
     private void addButton(String text, ActionListener listener) {
         JButton button = new JButton(text);
         buttonPad.add(button);
         button.addActionListener(listener);
     }
-    
-    private void updateDisplay(String text) {
-        display.setText(text);
-    }
-    
-    class StringTextAdder implements ActionListener {
-        
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if(e.getActionCommand().matches("[*+/-]")){
-                equationString += " " + e.getActionCommand() + " ";
-            }
-            else
-                equationString += e.getActionCommand();
-            updateDisplay(equationString);
-        }
-        
-    }
-    
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
     }
-    
+
 }
